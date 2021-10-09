839c26ecd45bcfd0ba9a63012e829f43a27f09bb
vad998 <57177263+vad998@users.noreply.github.com>
2021-10-09 14:49:47 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+import http from 'http';
+
+const CORS = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET,POST,DELETE,OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, x-test'
+};
+
+const server = http.createServer(async(request,response)=>{
+  if(request.url === '/result4/'){
+    let data = '';
+    
+    response.writeHead(200,{
+      'Content-Type':'application/json',
+      ...CORS,
+    });
+    
+    await request.on('data', function(chunk){
+      data += chunk;
+      }).on('end', () => {
+    });
+    
+    response.write(JSON.stringify({
+      "message":"itmo307694",
+      "x-result":headers,
+      "x-body":data
+    }));
+  }
+  
+  res.end();
+});
+
+server.listen(4321,()=>{console.log('Server is running')});
